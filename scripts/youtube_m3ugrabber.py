67ff196653056913960a7c277472a4d0a87368d6
HendryAng24 <31853285+HendryAng24@users.noreply.github.com>
2022-03-11 18:06:50 +0700

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -40,6 +40,8 @@ def grab(url):
             tuner += 5
     print(f"{link[start : end]}")
 
+print('#EXTM3U ')
+print(banner)
 #s = requests.Session()
 with open('../youtube_channel_info.txt') as f:
     for line in f:
