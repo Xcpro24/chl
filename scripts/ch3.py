669bf009d0b80aedf68ef915c2bc81fc16b10883
Xcpro <31853285+Xcpro24@users.noreply.github.com>
2022-11-11 11:22:49 +0700

    Update ch3.py

--- scripts/ch3.py
+++ scripts/ch3.py
@@ -23,7 +23,7 @@ def grab(url):
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
-        if 'http://' in response[end-tuner : end]:
+        if 'https://' in response[end-tuner : end]:
             link = response[end-tuner : end]
             start = link.find('https://')
             end = link.find('.m3u8') + 5
