c36bd02a563ef5494eb2fd55cd45412e000cc7cf
Xcpro <31853285+Xcpro24@users.noreply.github.com>
2023-11-04 11:22:12 +0700

    Update ch1.py

--- scripts/ch1.py
+++ scripts/ch1.py
@@ -13,12 +13,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://github.com/Xcpro24/chl/blob/main/assets/Eror404.mp4?raw=true')
+            print('https://iptvok.my.id/exp/vid.m3u8')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://github.com/Xcpro24/chl/blob/main/assets/Eror404.mp4?raw=true')
+            print('https://iptvok.my.id/exp/vid.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
